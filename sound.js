f416633287fddf80e05536f44efc43254528fa47
Simon Woolf <simon@ably.io>
2018-12-31 17:11:42 +0000

    Cleanup js files

--- sound.js
+++ sound.js
@@ -1,17 +1,11 @@
 var epsilon = 0.000001;
 var context = new AudioContext();
-// expose for debugging
-window.audioContext = context;
 
 function ding(startFreq) {
   var now = context.currentTime;
   var oscillator = context.createOscillator();
   oscillator.type = "sine";
   oscillator.frequency.setValueAtTime(startFreq, now);
-  //oscillator.frequency.setValueAtTime(startFreq, now + 1);
-  //oscillator.frequency.exponentialRampToValueAtTime(
-    //startFreq * 0.75, now + 6
-  //);
 
   var gainFilter = context.createGain();
   oscillator.connect(gainFilter);
